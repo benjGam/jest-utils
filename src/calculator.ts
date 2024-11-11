056cae650b61596fd10bc0db93f2c96e79537847
benjGam <mecopilive@gmail.com>
2024-11-11 23:24:46 +0100

    feat: implement 'toSub' method

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,8 @@ export class Calculator {
   public static add(x: number, y: number) {
     return x + y;
   }
+
+  public static sub(base: number, toSub: number[]) {
+    return toSub.reduce((p, c) => p - c, base);
+  }
 }
