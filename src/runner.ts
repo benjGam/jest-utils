1e3a375933f41a4cb9264e4de2dcbb47f4f97243
benjGam <mecopilive@gmail.com>
2024-11-11 22:38:29 +0100

    feat: implement function invokation method

--- src/runner.ts
+++ src/runner.ts
@@ -8,4 +8,16 @@ export class JestRunner {
     inputsAndOutputMap: Map<unknown, unknown>,
     logicToRunAfter: Function | undefined = undefined,
   ): void {}
+
+  private checkPrototypeOfInvokedFunction(functionToTest: Function) {
+    const expectedFunctionPrototype: Function | undefined =
+      this.classToInvoke[functionToTest.name];
+    if (
+      !expectedFunctionPrototype ||
+      functionToTest !== expectedFunctionPrototype
+    )
+      throw new Error(
+        `The prototype of tested function cannot be found in ${this.classToInvoke.name} class or do not correspond to defined one.`,
+      );
+  }
 }
