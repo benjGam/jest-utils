7f86363e79f00dd795130929ace3c677090673a2
benjGam <mecopilive@gmail.com>
2024-11-11 23:24:58 +0100

    tests: add tests for 'sub' method

--- test/calculator.spec.ts
+++ test/calculator.spec.ts
@@ -3,3 +3,7 @@ import { Calculator } from '../src/calculator';
 test(`Should return 10 for x = 5 & y = 5`, () => {
   expect(Calculator.add(5, 5));
 });
+
+test(`Shoud return 9 for base = 10 & toSub = [1]`, () => {
+  expect(Calculator.sub(10, [1])).toBe(9);
+});
